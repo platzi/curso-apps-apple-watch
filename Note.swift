bebf5b16ccb33701fa9c97c6ce9ce9b7f54ffde8
German Santos <germans@unam.mx>
2021-04-29 17:49:26 -0500

    Defining data model

---
+++ Note.swift
@@ -0,0 +1,16 @@
+//
+//  Note.swift
+//  Curso Apps Apple Watch WatchKit Extension
+//
+//  Created by Germán Santos Jaimes on 29/04/21.
+//
+
+import Foundation
+
+struct Note: Identifiable{
+    let id: UUID
+    let title: String
+    let creationDate: Date
+    
+    
+}
